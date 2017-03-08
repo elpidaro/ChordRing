29009028e959c5cf6cb0a8be230648e14272029c
elpidaro <rouka.elpida@gmail.com>
2017-03-08 12:02:29 +0200

    query, insert, delete

--- ChordRing/src/Node.java
+++ ChordRing/src/Node.java
@@ -3,10 +3,10 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Node extends Thread {
-	private int id = 0, successor, predecessor;
+	private int myid = 0, successor, predecessor;
 	private String myname; // "1", "2", ...
 	int ring_size;
-	Map<String, Integer> files = new HashMap<>();
+	Map<Integer, Integer> files = new HashMap<>();
 	
 
 	public Node(String name, int size) {
@@ -14,17 +14,23 @@ public class Node extends Thread {
 		ring_size = size;
 	}
 
-
+	private boolean iAmResponsibleForId (int id){
+		if ((id > predecessor && id <= myid) || (myid < predecessor && ((id < myid) || (id > predecessor)))){
+			return true;
+		}
+		else return false;
+	}
+	
 	public long getId() {
-		if (id != 0) return id;
+		if (myid != 0) return myid;
 		else {
 			try {
-				id = ChordRing.calculate_sha1(myname, ring_size);
+				myid = ChordRing.calculate_sha1(myname, ring_size);
 			} catch (NoSuchAlgorithmException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-			return id;
+			return myid;
 		}
 	}
 
@@ -52,6 +58,47 @@ public class Node extends Thread {
 		this.ring_size = ring_size;
 	}
 	
+	int query(Command c) throws NoSuchAlgorithmException{
+		int song_id = ChordRing.calculate_sha1(c.key, ring_size);
+		int value = -1; // i am not responsible for song
+		if (iAmResponsibleForId(song_id)){
+			value = -2; // i am responsible, song doesn't exist 
+			if (files.containsKey(song_id)){
+				value = files.get(song_id); // song exists in my list --> value >= 0 returned
+			}
+		}
+		return value;
+	}
+	
+	int insert(Command c) throws NoSuchAlgorithmException{
+		int song_id = ChordRing.calculate_sha1(c.key, ring_size);
+		int answer = 0; // i am not responsible for song
+		if (iAmResponsibleForId(song_id)){
+			if (files.containsKey(song_id)){
+				// update
+				files.replace(song_id, c.value);
+			}
+			else{
+				// insert
+				files.put(song_id, c.value);
+			}
+			answer = 1; // I did the insert
+		}
+		return answer;
+	}
+	
+	int delete (Command c) throws NoSuchAlgorithmException{
+		int song_id = ChordRing.calculate_sha1(c.key, ring_size);
+		int answer = 0; // i am not responsible for song
+		if (iAmResponsibleForId(song_id)){
+			if (files.containsKey(song_id)){
+				// delete
+				files.remove(song_id);
+			}
+			answer = 1; // I did the deletion
+		}
+		return answer;
+	}
 	
 	
 }
