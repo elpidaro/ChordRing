cc0d7ad9c10dad15fbf71f99ee8dce5990749d7c
fivosts <fivos_ts@hotmail.com>
2017-03-14 14:27:59 +0200

    oi entoles me mikra

--- ChordRing/Replicationsrc/ChordRing.java
+++ ChordRing/Replicationsrc/ChordRing.java
@@ -16,13 +16,13 @@ import java.util.concurrent.ThreadLocalRandom;
 //import java.lang.StringBuilder;
 /*
  * COMMAND EXAMPLES:
- * INSERT,eimaste omadara,100
- * INSERT,Love is in the air,97
- * QUERY,eimaste omadara
- * DEPART,708
- * DELETE,Love is in the air
- * DELETE,akuro_pou_den_uparxei
- * QUERY,*
+ * insert,eimaste omadara,100
+ * insert,Love is in the air,97
+ * query,eimaste omadara
+ * depart,708
+ * delete,Love is in the air
+ * delete,akuro_pou_den_uparxei
+ * query,*
  */
 
 public class ChordRing {
@@ -79,14 +79,14 @@ public class ChordRing {
 		
 		BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
 		//System.out.println("Please enter the <number of desired nodes>: ");
-		int number_of_nodes = 5;//Integer.parseInt(input.readLine());
+		int number_of_nodes = 10;//Integer.parseInt(input.readLine());
 		//System.out.println("Please enter the <log of ring size>: ");
-		int M = 10; //Integer.parseInt(input.readLine());
+		int M = 6; //Integer.parseInt(input.readLine());
 		double ring = Math.pow(2,M);
 		int ring_size = (int) Math.round(ring);
 		int globalc; // global node counter
 		//Number of replicas
-		int number_of_replicas =3;
+		int number_of_replicas =5;
 		List<Node> nodelist = new ArrayList<Node>();
 		
 		System.out.printf("Initial number of nodes: %d ring size: %d replication factor: %d\n",number_of_nodes,ring_size,number_of_replicas);
@@ -112,8 +112,8 @@ public class ChordRing {
 			System.out.println("Type your command: ");
 			String command = input.readLine();	
 			//System.out.println("command IS: "+ command);
-			String option = command.split(",")[0]; //INSERT,QUERY,DELETE,JOIN,DEPART
-			if (option.equals("INSERT") || option.equals("DELETE") || option.equals("QUERY")) {
+			String option = command.split(",")[0]; //insert,query,delete,join,depart
+			if (option.equals("insert") || option.equals("delete") || option.equals("query")) {
 				int len = nodelist.size();
 				int randomNum = ThreadLocalRandom.current().nextInt(1, len);
 				Node init = nodelist.get(randomNum);
@@ -126,7 +126,7 @@ public class ChordRing {
 				}
 			
 			}
-			else if (option.equals("DEPART")){
+			else if (option.equals("depart")){
 				Node node_to_delete = null;
 				int idToDelete = Integer.parseInt(command.split(",")[1]);
 				// Check if node with this ID exists
@@ -134,7 +134,7 @@ public class ChordRing {
 					if (n.getmyId() == idToDelete){
 						node_to_delete = n;
 						// send the depart massage
-						main_forward_to("DEPART\n",number_of_replicas, node_to_delete.getMyname(), node_to_delete.getmyPort());
+						main_forward_to("depart\n",number_of_replicas, node_to_delete.getMyname(), node_to_delete.getmyPort());
 						try {
 							Thread.sleep(4000);
 						} catch (InterruptedException e) {
@@ -152,13 +152,13 @@ public class ChordRing {
 					fix_nodes(nodelist);
 				}
 			}
-			else if (option.equals("JOIN")){
+			else if (option.equals("join")){
 				Node n = new Node("localhost",Integer.toString(globalc), ring_size,number_of_replicas);
 				nodelist.add(n);
 				fix_nodes(nodelist);
 				n.start();
-				// sends JOIN-successorID
-				main_forward_to("JOIN-"+n.successor.getmyId() +"\n", number_of_replicas, nodelist.get(0).getMyname(), nodelist.get(0).getmyPort());
+				// sends join-successorID
+				main_forward_to("join-"+n.successor.getmyId() +"\n", number_of_replicas, nodelist.get(0).getMyname(), nodelist.get(0).getmyPort());
 				try {
 					Thread.sleep(8000);
 				} catch (InterruptedException e) {
